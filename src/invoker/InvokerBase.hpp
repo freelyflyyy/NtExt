6f90647f5115b5b7fb89cf57c6790ea0197c679c
如风 <3262150477@qq.com>
2026-04-14 01:48:18 +0800

    fix : 重写底层汇编，修复可能导致的堆栈不平衡

--- src/invoker/InvokerBase.hpp
+++ src/invoker/InvokerBase.hpp
@@ -11,9 +11,12 @@ namespace NtExt {
 			0x56,                                                        // push rsi
 			0x57,                                                        // push rdi
 			0x41, 0x54,                                                  // push r12
-		};
+		}; 
+		
 		static constexpr BYTE restore_env[] = {
-			0x48, 0x8D, 0x65, 0xE0,                                      // lea rsp,[ rbp - 32 ]
+			0x48, 0x89, 0xC2,                                            // mov rdx, rax
+			0x48, 0xC1, 0xEA, 0x20,                                      // shr rdx, 32
+			0x48, 0x8D, 0x65, 0xE0,                                      // lea rsp, [rbp - 0x20]
 			0x41, 0x5C,                                                  // pop r12
 			0x5F,                                                        // pop rdi
 			0x5E,                                                        // pop rsi
@@ -23,29 +26,37 @@ namespace NtExt {
 		};
 
 		static constexpr BYTE prepare_env[] = {
-			0x48, 0xBE, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // mov rsi, _pParam
-			0x49, 0xBC, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // mov r12, _argC
-			0x4C, 0x89, 0xE0,                                            // mov rax, r12
-			0x49, 0x83, 0xFC, 0x04,                                      // cmp r12, 4
-			0x7F, 0x07,                                                  // jg +7
-			0x48, 0xC7, 0xC0, 0x04, 0x00, 0x00, 0x00,                    // mov rax, 4
-			0x48, 0xC1, 0xE0, 0x03,                                      // shl rax, 3
-			0x48, 0x29, 0xC4,                                            // sub rsp, rax
-			0x48, 0x83, 0xE4, 0xF0,                                      // and rsp, 0xFFFFFFF0
-			0x48, 0x8B, 0x0E,                                            // mov rcx, [rsi]
-			0x48, 0x8B, 0x56, 0x08,                                      // mov rdx, [rsi + 8]
-			0x4C, 0x8B, 0x46, 0x10,                                      // mov r8, [rsi + 16]
-			0x4C, 0x8B, 0x4E, 0x18,                                      // mov r9, [rsi + 24]
-			0x49, 0x83, 0xFC, 0x04,                                      // cmp r12, 4
-			0x7E, 0x17,                                                  // jle _ready
-			0x49, 0xC7, 0xC3, 0x04, 0x00, 0x00, 0x00,                    // mov r11, 4
-			// _loop:
-			0x4A, 0x8B, 0x04, 0xDE,                                      // mov rax, [rsi + r11 * 8]
-			0x4A, 0x89, 0x04, 0xDC,                                      // mov [rsp + r11 * 8], rax
-			0x49, 0xFF, 0xC3,                                            // inc r11
-			0x4D, 0x39, 0xE3,                                            // cmp r11, r12
-			0x7C, 0xF0                                                   // jl _loop
-			// _ready:
+			0x48, 0xBF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // mov rdi, _arg[]
+			0x48, 0xB8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // mov rax, _argC 
+			0x48, 0x8B, 0x0F,                                            // mov rcx, qword ptr [rdi]
+			0x48, 0x83, 0xC7, 0x08,                                      // add rdi, 8
+			0x48, 0xFF, 0xC8,                                            // dec rax
+			0x48, 0x83, 0xF8, 0x00,                                      // cmp rax, 0
+			0x74, 0x48,                                                  // je _prepare_call
+			0x48, 0x8B, 0x17,                                            // mov rdx, qword ptr [rdi]
+			0x48, 0x83, 0xC7, 0x08,                                      // add rdi, 8
+			0x48, 0xFF, 0xC8,                                            // dec rax
+			0x48, 0x83, 0xF8, 0x00,                                      // cmp rax, 0
+			0x74, 0x38,                                                  // je _prepare_call
+			0x4C, 0x8B, 0x07,                                            // mov r8, qword ptr [rdi]
+			0x48, 0x83, 0xC7, 0x08,                                      // add rdi, 8
+			0x48, 0xFF, 0xC8,                                            // dec rax
+			0x48, 0x83, 0xF8, 0x00,                                      // cmp rax, 0
+			0x74, 0x28,                                                  // je _prepare_call	
+			0x4C, 0x8B, 0x0F,                                            // mov r9, qword ptr [rdi]
+			0x48, 0x83, 0xC7, 0x08,                                      // add rdi, 8
+			0x48, 0xFF, 0xC8,                                            // dec rax
+			0x48, 0x83, 0xF8, 0x00,                                      // cmp rax, 0
+			0x74, 0x18,                                                  // je _prepare_call
+			0xA8, 0x01,                                                  // test al, 1	
+			0x74, 0x04,                                                  // je _even_args
+			0x48, 0x83, 0xEC, 0x08,                                      // sub rsp, 8	
+			0x48, 0x8D, 0x7C, 0xC7, 0xF8,                                // lea rdi, [rdi + rax*8 - 8]	
+			0xFF, 0x37,                                                  // push qword ptr [rdi]	
+			0x48, 0x83, 0xEF, 0x08,                                      // sub rdi, 8	
+			0x48, 0xFF, 0xC8,                                            // dec rax	
+			0x75, 0xF5,                                                  // jnz _push_loop	
+			0x48, 0x83, 0xEC, 0x20                                       // sub rsp, 0x20
 		};
 	}
 
